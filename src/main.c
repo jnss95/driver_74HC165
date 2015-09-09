f95d70d08c686ee334b8c27ec444bfae6c505416
Xeranor <jonas.schulz.12@gmail.com>
2015-09-09 21:08:13 +0200

    Temporary

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
+//This file is only to debug the library
 #include "main.h"
+#include <util/delay.h>
 
 int main(void)
 {
+	DDRB = 0xff;
+	PORTB = 0xff;
 	while(1){
+		_delay_ms(1000);
+		PORTB = 0x00;
+		_delay_ms(1000);
+		PORTB = 0xff;
 	}
 	return 1;
 }
