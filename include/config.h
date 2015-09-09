38057f1379ec0f39222f2cb7109b6c45d97fb9c3
Xeranor <jonas.schulz.12@gmail.com>
2015-09-09 16:21:12 +0200

    Added config file with pin and port layout

---
+++ include/config.h
@@ -0,0 +1,18 @@
+#include <avr/io.h>
+
+#define PORT_CLK PORTD
+#define DDR_CLK DDRD
+#define BIT_CLK PD3
+
+#define PORT_CLK_INH PORTD
+#define DDR_CLK_INH DDRD
+#define BIT_CLK_INH PD5
+
+#define PORT_LD PORTD
+#define DDR_LD DDRD
+#define BIT_LD PD2
+
+#define PORT_SER_IN PORTD
+#define DDR_SER_IN DDRD
+#define PIN_SER_IN PIND
+#define BIT_SER_IN PD4
